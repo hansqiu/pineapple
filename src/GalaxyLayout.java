e76f191e6a5a7ac4ea54091b32021beb76b9b207
hansqiu <34635053+hansqiu@users.noreply.github.com>
2018-05-04 14:34:35 -0700

    Add files via upload

---
+++ src/GalaxyLayout.java
@@ -0,0 +1,32 @@
+/**
+ * A concrete class implementing galaxy theme for BoardLayout interface 
+ * @author Team Pineapple
+ * CS 151 Spring 2018
+ */
+
+public class GalaxyLayout implements BoardLayout 
+{
+	/**
+	 * To return a path of a stone image for Galaxy Theme
+	 */
+	public String getStonePic() {
+		return "image/GalaxyStone.png";
+	}
+
+	/**
+	 * To return a path of a pit image for Galaxy Theme
+	 */
+	public String getPitPic() 
+	{
+		return "image/GalaxyPit.png";
+	}
+
+	/**
+	 * To return a path of a mancala image for Galaxy Theme
+	 */
+	public String getMancalaPic() 
+	{
+		return "image/GalaxyMancala.png";
+	}
+
+}
