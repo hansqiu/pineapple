2094bac618117c3bb5b409c761ca3d38baa05d1b
hansqiu <34635053+hansqiu@users.noreply.github.com>
2018-05-05 14:26:57 -0700

    Add files via upload

---
+++ CS151FinalProject3/src/Mancala.java
@@ -0,0 +1,87 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+
+import javax.swing.ImageIcon;
+
+
+/** 
+ * This class contains the methods pertaining to functionality for the game and 
+ * the data for a given configuration and moment of gameplay.
+ * 
+ */
+
+public class Mancala extends Pit {
+    
+	/**
+     * Mancala class constructor - creates a mancala pit with the given parameters
+     * @param n - the number of marbles to initialize the pit with
+     * @param index - the location of the mancala
+     * @param player - the Player this mancala belongs to
+     * @param b - a concrete implementation of BoardStyle determining
+     * the shape of the pits to be used in the game
+     */
+    public Mancala(int n, int index, Player player, BoardLayout b) {
+        super(n, index, player, b);
+    }
+    
+    /**
+	 *Get the player the pit belongs to
+	 *@return the pit's player
+	 */
+    public Player getPlayer() {
+        return super.getPlayer();
+    }
+    
+    /**
+	 *Change the number of marbles in the current pit
+	 *@param n number of marbles to be set to
+	 */
+    public void setMarbles(int n) {
+        super.setMarbles(n);
+    }
+    
+    /**
+	 *Get the number of marbles in the current pit
+	 *@return the pit's marbles
+	 */
+    public int getMarbles() {
+        return super.getMarbles();
+    }
+    
+    /**
+	 *Get the index or location of each pit
+	 *@return the location of each pit
+	 */
+    public int getIndex() {
+        return super.getIndex();
+    }
+    
+    /**
+	 *Check if the pit is empty or not
+	 *@return true if the pit is empty and false otherwise
+	 */
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    /**
+	 *Get the shape of the pit to be drawn
+	 *@param b - the board style determining the shape
+	 *@return the Shape based on the board style
+	 */
+  
+    public String getPitImagePath(BoardLayout b)
+	{
+		return b.getMancalaPic(getPlayer());
+	}
+    /**
+	 *Get the player the pit belongs to
+	 *@param g graphics object used to draw shape
+	 */
+    public void paintComponent(Graphics g) 
+    {   
+		ImageIcon i = new ImageIcon(this.getPitImagePath(layout));
+		i.paintIcon(this, g, 10, 80);
+    }
+}
---
+++ CS151FinalProject3/src/MancalaTester.java
@@ -0,0 +1,106 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+public class MancalaTester
+{
+	static JPanel mainPanel;
+	static JButton threeWood;
+	static JButton fourWood;
+	static JButton threeGalaxy;
+	static JButton fourGalaxy;
+	static JFrame mainFrame;
+
+	public static void main(String[] args)
+	{
+
+		mainFrame = new JFrame("CS 151 Team Pineapple Spring 2018");
+
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout(2,2,2,2));
+
+		threeWood = new JButton("Wood Theme with 3 stones");
+		fourWood = new JButton("Wood Theme with 4 stones");
+
+
+		threeGalaxy = new JButton("Galaxy Theme with 3 stones");
+		fourGalaxy = new JButton("Galaxy Theme with 4 stones");
+
+		mainPanel.add(threeWood);
+		mainPanel.add(fourWood);
+		mainPanel.add(threeGalaxy);
+		mainPanel.add(fourGalaxy);
+
+		mainFrame.add(mainPanel);
+		mainFrame.setSize(400, 400);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+
+		threeWood.addActionListener(new chooseOne());
+		fourWood.addActionListener(new chooseOne());
+		threeGalaxy.addActionListener(new chooseOne());
+		fourGalaxy.addActionListener(new chooseOne());
+	}
+	
+	static class chooseOne implements ActionListener
+	{
+
+		public void actionPerformed (ActionEvent e)
+		{
+			mainFrame.setVisible(false);
+			WoodLayout wl = new WoodLayout();
+			GalaxyLayout gl = new GalaxyLayout();
+			threeWood.addMouseListener(new MouseAdapter() 
+			{
+				@Override
+				public void mouseClicked(MouseEvent e) 
+				{
+					super.mouseClicked(e); //To change body of generated methods, choose Tools | Templates.
+					Board b = new Board(wl);
+					BoardView bV=new BoardView(b);
+					b.addChangeListener(bV);
+					b.fillBoard(3);
+				};
+			});
+			fourWood.addMouseListener(new MouseAdapter() 
+			{
+				@Override
+				public void mouseClicked(MouseEvent e) 
+				{
+					super.mouseClicked(e); //To change body of generated methods, choose Tools | Templates.
+					Board b = new Board(wl);
+					BoardView bV=new BoardView(b);
+					b.addChangeListener(bV);
+					b.fillBoard(4);
+				};
+			});
+			threeGalaxy.addMouseListener(new MouseAdapter() 
+			{
+				@Override
+				public void mouseClicked(MouseEvent e) 
+				{
+					super.mouseClicked(e); //To change body of generated methods, choose Tools | Templates.
+					Board b = new Board(gl);
+					BoardView bV=new BoardView(b);
+					b.addChangeListener(bV);
+					b.fillBoard(3);
+				};
+			});
+			fourGalaxy.addMouseListener(new MouseAdapter() 
+			{
+				@Override
+				public void mouseClicked(MouseEvent e) 
+				{
+					super.mouseClicked(e); //To change body of generated methods, choose Tools | Templates.
+					Board b = new Board(gl);
+					BoardView bV=new BoardView(b);
+					b.addChangeListener(bV);
+					b.fillBoard(4);
+				};
+			});
+		}
+	}
+}
