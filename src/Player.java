e4ef222cd925e1735b6c5287e43f0d5f9b87072d
hansqiu <34635053+hansqiu@users.noreply.github.com>
2018-05-05 16:17:58 -0700

    Delete Player.java

--- src/Player.java
+++
@@ -1,9 +0,0 @@
-/** 
- * This class contains the information pertaining to functionality for the game and 
- * the data for a given configuration and moment of gameplay.
- * 
- */
-
-public enum Player{
-    A,B
-}
